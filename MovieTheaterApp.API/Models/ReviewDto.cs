abb588de7e5fc809e6a7847a2ce0385f71753d0c
darknath <nathan.preel@gmail.com>
2017-07-12 19:12:29 +0300

    add review related dto's

---
+++ MovieTheaterApp.API/Models/ReviewDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTheaterApp.API.Models
+{
+    public class ReviewDto
+    {
+        public int Id { get; set; }
+                
+        public double? Score { get; set; }
+
+        public string Text { get; set; }
+
+        public int MovieId { get; set; }
+
+        public UserDto User { get; set; }
+    }
+}
---
+++ MovieTheaterApp.API/Models/ReviewDtoAdd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTheaterApp.API.Models
+{
+    public class ReviewDtoAdd
+    {
+        [Range(0.5d, 10.0d)]
+        public double? Score { get; set; }
+
+        public string Text { get; set; }
+
+        public int MovieId { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
