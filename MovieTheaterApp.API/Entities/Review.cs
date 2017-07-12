19fe89c3541986048cfbb9cea21c79b66a37f8b1
darknath <nathan.preel@gmail.com>
2017-07-12 19:11:51 +0300

    add review entity

---
+++ MovieTheaterApp.API/Entities/Review.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTheaterApp.API.Entities
+{
+    public class Review
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Range(0.5d, 10.0d)]
+        public double? Score { get; set; }
+
+        public string Text { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+
+        [ForeignKey("Movie")]
+        public int MovieId { get; set; }
+
+        public Movie Movie { get; set; }
+    }
+}
